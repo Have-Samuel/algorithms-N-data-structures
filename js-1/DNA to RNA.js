98214af75b7082b2285e7c171a6f72dfff37615c
Have Samuel <samiehave9@gmail.com>
2024-04-11 16:59:21 +0300

    Returning the Returning the RNA sequence by replacing all 'T' with 'U'

--- js-1/DNA to RNA.js
+++ js-1/DNA to RNA.js
@@ -1,5 +1,12 @@
 let dna = "GCAT";
 function DNAtoRNA(dna) {
   // create a function which returns an RNA sequence from the given DNA sequence
-  return dna.replace(/T/g, "U");
+  // Returning the RNA sequence by replacing all 'T' with 'U'
+  // Using the replace method with a regular expression to replace all 'T' with 'U'
+  // The 'g' flag is used to replace all occurrences of 'T' in the string
+  // We didn't use the 'i' flag because the DNA sequence is case-sensitive
+  // The 'i' flag is used to perform case-insensitive matching
+  // The 'g' flag is used to perform a global match (find all matches rather than stopping after the first match)
+  // The 'gi' flag is used to perform a global, case-insensitive match
+  return dna.replace(/T/gi, "U");
 }
