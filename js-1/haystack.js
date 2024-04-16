760f43d28f32ba1d78b6aa04505f3ee5eec9595d
Have Samuel <samiehave9@gmail.com>
2024-04-16 23:01:24 +0300

    Solving the Chanllenge

--- js-1/haystack.js
+++ js-1/haystack.js
@@ -1,3 +1,9 @@
 // looping through the Array
 let haystack = ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"];
 
+for (let i = 0; i < haystack.length; i++) {
+    if (haystack[i] === "needle") {
+        console.log("Found the needle at position " + i);
+        
+    }
+}
