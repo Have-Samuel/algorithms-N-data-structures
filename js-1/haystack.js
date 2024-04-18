7c675124d4a1f00d6c43cfe2aadc8e6b425ac8e1
Have Samuel <samiehave9@gmail.com>
2024-04-18 16:40:58 +0300

    Finding the position of any item in the ARRAy

--- js-1/haystack.js
+++ js-1/haystack.js
@@ -2,8 +2,8 @@ let arry = ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"];
 
 const findNeedle = (arry) => {
     for (let i = 0; i < arry.length; i++) {
-        if (arry[i] === 'needle') {
-            console.log(`found the needle at position`)
+        if (arry[i] === 'hay') {
+            console.log(`found the needle at position, ${i}`)
         }
     }
     return arry;
