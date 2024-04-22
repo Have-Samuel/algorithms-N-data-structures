c0bf56077ab0ceae13ff6eaef5b8e6787c9e686f
Have Samuel <samiehave9@gmail.com>
2024-04-22 16:59:53 +0300

    Checking

--- js-1/thirteenth.js
+++ js-1/thirteenth.js
@@ -2,3 +2,9 @@ function fridayTheThirteenths(start, end) {
 
 
 }
+
+// const valentines = new Date("1990-04-26");
+// const day = valentines.getDay();
+// const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"/* , … */];
+
+// console.log(dayNames[day]);
