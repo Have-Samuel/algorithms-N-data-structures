571a9cf636520d6a6db9ddfc17f2edaac3a02770
Have Samuel <samiehave9@gmail.com>
2024-04-22 16:06:52 +0300

    Console

--- js-1/haystack.js
+++ js-1/haystack.js
@@ -4,10 +4,10 @@ let arry = ["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"];
 const findNeedle = (arry) => {
     for (let i = 0; i < arry.length; i++) {
         if (arry[i] === 'needle') {
-            console.log(`found the needle at position, ${i}`)
+            return `found the needle at position, ${i}`;
         }
     }
-    return arry;
+    // return arry;
 }
 console.log(findNeedle(arry));
-
+// findNeedle(arry);
