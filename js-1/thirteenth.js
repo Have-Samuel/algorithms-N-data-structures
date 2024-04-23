c23a78b8507aa3750a19856e29d20d5f8d90ad4c
Have Samuel <samiehave9@gmail.com>
2024-04-23 22:56:14 +0300

    Checking for another solution

--- js-1/thirteenth.js
+++ js-1/thirteenth.js
@@ -1,20 +1,9 @@
 function fridayTheThirteenths(start, end) {
   // let satrt = new Date(start);
   // let end = new Date(end);
-
-  let friday13 = [];
-  // loop through the years
-  // let i = start;
-  for (let i = start; i <= end; i++) {
-    let date = new Date(i, 0, 13);
-    if (date.getDay() === 5) {
-      friday13.push(date);
-    }
-  }
-  return friday13;
+ 
 }
 
-console.log(fridayTheThirteenths(1990, 2020));
 
 // const valentines = new Date("1990-04-26");
 // const day = valentines.getDay();
