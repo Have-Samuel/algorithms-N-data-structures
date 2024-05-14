462b63e06e52e677c8a234f4b0bac63b0c2c0b9d
samuel <samiehave9@gmail.com>
2024-05-14 16:40:12 +0300

    Editing

--- js-1/fridaythethirteenths.js
+++ js-1/fridaythethirteenths.js
@@ -3,12 +3,12 @@ function fridayTheThirteenths(start, end) {
     let friday13 = [];
     for (let year = start; year <= end; year++) {
         for (let month = 0; month < 12; month++) {
-        let date = new Date(year, month, 13);
-        if (date.getDay() === 2) {
+            let date = new Date(year, month, 13);
+            if (date.getDay() === 2) {
             friday13.push(date);
+            }
         }
-}
-};
+    }
 return friday13;
 }
 console.log(fridayTheThirteenths(2018, 2020));
