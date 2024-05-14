edcd66da5245ec69c26b1967e4a71efbd8a0ff66
samuel <samiehave9@gmail.com>
2024-05-14 16:19:25 +0300

    Function that Counts down the Years

--- js-1/fridaythethirteenths.js
+++ js-1/fridaythethirteenths.js
@@ -1,4 +1,14 @@
 function fridayTheThirteenths(start, end) {
-// Your code here
-
+//  let friday13 = 0;
+    let friday13 = [];
+    for (let year = start; year <= end; year++) {
+        for (let month = 0; month < 12; month++) {
+        let date = new Date(year, month, 13);
+        if (date.getDay() === 2) {
+            friday13.push(date);
+        }
 }
+};
+return friday13;
+}
+console.log(fridayTheThirteenths(2018, 2020));
