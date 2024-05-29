1a7d2a902816820cbadadc24832a046e98003260
samuel <samiehave9@gmail.com>
2024-05-29 12:06:18 +0300

    Function

---
+++ js-1/markNjohn.js
@@ -0,0 +1,14 @@
+let markMass = 78;
+let markHeight = 1.69;
+let johnMass = 92;
+let johnHeight = 1.95;
+
+const  markBMI = markMass / (markHeight * markHeight);
+78 / (1.69 * 1.69);
+console.log(markBMI);
+
+const johnBMI = johnMass / (johnHeight * johnHeight);
+92 / (1.95 * 1.95);
+console.log(johnBMI);
+
+// console.log(markBMI, johnBMI);
