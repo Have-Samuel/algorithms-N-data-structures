3ba6cf377315e4ca4de61a0cd0b129e790352f57
samuel <samiehave9@gmail.com>
2024-05-30 09:33:11 +0300

    Changing the Scores the get different conditions

--- js-1/challenge2.js
+++ js-1/challenge2.js
@@ -1,7 +1,8 @@
+// Keep changing the scores to test the different conditions
 const johnTeamAvg = (89 + 120 + 103) / 3;
 // johnTeamAvg = 104
-const mikeTeamAvg = (116 + 94 + 123) / 3;
-// mikeTeamAvg = 111
+const mikeTeamAvg = (95  + 94 + 123) / 3;
+// mikeTeamAvg = 104
 const maryTeamAvg = (97 + 134 + 105) / 3;
 // maryTeamAvg = 112
 
@@ -9,4 +10,6 @@ if (johnTeamAvg > mikeTeamAvg) {
     console.log('John\'s team wins with an average of ' + johnTeamAvg);
 } else if (mikeTeamAvg > johnTeamAvg) {
     console.log('Mike\'s team wins with an average of ' + mikeTeamAvg);
+} else {
+    console.log('It\'s a draw');
 }
