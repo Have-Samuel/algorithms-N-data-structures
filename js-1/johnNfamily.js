f82827c02cd8777f0148363b925e9b2c3ab5d273
samuel <samiehave9@gmail.com>
2024-06-07 10:14:28 +0300

    Tipcalculator Function

--- js-1/johnNfamily.js
+++ js-1/johnNfamily.js
@@ -1,3 +1,23 @@
-const calculator = function () {
-    
+function tipCalculator(bill) {
+    let percentage;
+    if (bill < 50) {
+        percentage = 0.2;
+    } else if (bill >= 50 && bill < 200) {
+        percentage = 0.15;
+    } else {
+        percentage = 0.1;
+    }
+    return percentage *bill;    
 }
+
+let bills = [124, 48, 268];
+console.log(tipCalculator(40));
+
+const finalTip = [tipCalculator(bills[0]),
+                 tipCalculator(bills[1]),
+                 tipCalculator(bills[2])];
+
+const sum = [finalTip[0] + bills[0],
+             finalTip[1] + bills[1],
+             finalTip[2] + bills[2]]
+console.log(finalTip, sum);
