1f0e51b23b6a00ebd83663e48c25e23cd9e09269
samuel <samiehave9@gmail.com>
2024-06-16 15:49:53 +0300

    Removing the Console and using the functions in the condition statement

--- js-1/obj.js
+++ js-1/obj.js
@@ -1,25 +1,28 @@
 const mark = {
     name: 'Mark',
-    mass: 98,
+    mass: 78,
     height: 1.69,
     markBMI: function() {
         this.bmi = this.mass / (this.height * this.height);
-        return this.markBMI;
+        return this.bmi;
     }
 };
 
-const John = {
+const john = {
     name: 'John',
     mass: 92,
     height: 1.95,
     johnBMI: function() {
         this.bmi = this.mass / (this.height * this.height);
-        return this.johnBMI;
+        return this.bmi;
     }
 };
 
-
-console.log(mark.markBMI(), John.johnBMI());
-
-
+if (john.johnBMI() > mark.markBMI()) {
+    console.log(`${john.name} has a higher BMI of ${john.bmi}`);
+} else if (mark.bmi > john.bmi) {
+    console.log(`${mark.name} has a higher BMI of ${mark.bmi}`);
+} else {
+    console.log(`They have the same BMI`);
+}
 
